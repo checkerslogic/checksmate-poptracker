bef6903c88f0a9e9197f0ce9eee02d02e981d472
checkerslogic <syriticus@gmail.com>
2025-02-06 13:37:33 -0800

    gettin mad

--- scripts/autotracking/item_mapping.lua
+++ scripts/autotracking/item_mapping.lua
@@ -1,7 +1,7 @@
 -- use this file to map the AP item ids to your items
 -- first value is the code of the target item and the second is the item type override. The third value is an optional increment multiplier for consumables. (feel free to expand the table with any other values you might need (i.e. special initial values, etc.)!)
 -- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
-BASE_ITEM_ID = 0
+BASE_ITEM_ID = 4901000
 ITEM_MAPPING = {
 	[BASE_ITEM_ID + 00000] = { { "toggle" } },
 	[BASE_ITEM_ID + 00001] = { { "progressive" } },
